f8d7af26662ff0cb13b713b83e98b950a8b557ef
kevgilmore <kgilmore.me@gmail.com>
2023-11-14 23:27:13 +0000

    test new hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,35 +2,32 @@ import heroImg from '../img/hero1.jpg';
 
 export const Hero = () => {
   return (
-    <section className="h-screen w-4/5 mx-auto mt-6 md:mt-24">
-      <div className="pt-8 grid grid-cols-1 md:grid-cols-10 gap-12 md:gap-24 mx-8 mx-auto ">
-        <div className="pt-8 md:pt-20 mt-8 md:col-span-4 text-center md:text-left">
-
-
-          <h1 className="font-sans font-bold pt-4 text-5xl md:text-7xl text-zinc-800 tracking-relaxed ">
-          Get the {' '}
-          <span className="bg-gradient-to-r from-pink-500 via-purple-500 to-[#2e0553] text-transparent bg-clip-text">
-            Concierge
-          </span>
-          {' '} Treatment
-        </h1>
-
-          <p className="font-regular text-xl text-zinc-500 pt-4 mt-2 ">
-            Effortless greeting card subscriptions make you the person who always remembers.
-          </p>
-          <div className="flex flex-row mx-auto justify-center md:justify-start mt-4 pt-4">
-            <div className="typeform-btn" data-tf-live="01HF7K4E0F2YN6BSXTPBNGYVRA"></div>
-          </div>
-        </div>
-        <div className=" w-full rounded-xl md:col-span-6">
-          <img
-            className="object-cover h-full rounded-xl"
-            src={heroImg}
-            alt='hero'
-          />
-        </div>
+<section className="h-screen w-4/5 mx-auto mt-6 md:mt-24">
+  <div className="pt-8 grid grid-cols-1 md:grid-cols-10 gap-12 md:gap-24 mx-8 mx-auto ">
+    <div className="pt-8 md:pt-20 mt-8 md:col-span-4 text-center md:text-left">
+      <h1 className="font-medium pt-4 text-5xl md:text-7xl  text-zinc-800 tracking-relaxed ">
+        Build fast dashboards in minutes
+      </h1>
+      <p className="font-regular text-xl text-zinc-500 pt-4 mt-2 ">
+        Get started now with a free trial to try out our free app.
+      </p>
+      <div className="flex flex-row mx-auto justify-center md:justify-start mt-4 pt-4">
+        <button
+          className="text-white bg-indigo-500 hover:bg-indigo-700 transition focus:ring-4  focus:ring-blue-300 font-semibold rounded-lg text-base md:text-lg  px-8 py-4 mr-2 mb-2 focus:outline-none"
+          type="button"
+        >
+          Get Started
+        </button>
       </div>
-    </section>
+    </div>
+    <div className=" w-full rounded-xl md:col-span-6">
+      <img
+        className="object-cover h-full rounded-xl"
+        src="https://source.unsplash.com/PNodyzJcccA/1700x950"
+      />
+    </div>
+  </div>
+</section>
   )
 }
 
