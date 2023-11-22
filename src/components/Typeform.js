ebf2838da27dd2e3a7727cb009f2ecafe5d5c8c1
kevgilmore <kgilmore.me@gmail.com>
2023-11-22 21:18:53 +0000

    add GA event for button press

--- src/components/Typeform.js
+++ src/components/Typeform.js
@@ -1,6 +1,8 @@
 import { SliderButton  } from '@typeform/embed-react'
+import ReactGA from 'react-ga4'
 
 const Typeform = () => {
+  const formId = "Pe3D5X40"
 
   const buttonStyle = {
     padding: '10px 20px',
@@ -14,10 +16,21 @@ const Typeform = () => {
   }
 
   return (
-    <SliderButton id="Pe3D5X40" style={buttonStyle}>
+    <SliderButton
+      id={formId}
+      size={60}
+      style={buttonStyle}
+      onReady={() => {
+        ReactGA.event({
+          category: 'User interaction',
+          action: 'Typeform button pressed',
+          label: formId,
+        })
+      }}
+    >
       Subscribe Now
     </SliderButton>
   )
 };
 
-export default Typeform;
+export default Typeform
