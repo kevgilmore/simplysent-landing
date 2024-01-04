d65e993f9871af9403127bac31d1f254a28064fa
kevgilmore <kgilmore.me@gmail.com>
2024-01-04 19:21:28 +0000

    add jsconfig.json

--- src/components/feedback/types/FreeText.js
+++ src/components/feedback/types/FreeText.js
@@ -1,9 +1,24 @@
-const FreeText = () => {
-    return (
-        <>
-            <textarea className="rounded mt-5 h-70 p-1 placeholder:italic placeholder:text-slate-400" placeholder="Type anything..."></textarea>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">Submit</button>
-        </>
-    )}
+const FreeText = ({handleSubmit}) => {
+
+  const handleInput = (event) => {
+    handleSubmit("hello")
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleInput}>
+      <input 
+        name="message"
+        className="rounded mt-5 h-70 p-1 placeholder:italic placeholder:text-slate-400" 
+        placeholder="Type anything...">
+      </input>
+
+      <button 
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded" 
+        type="submit">
+          Submit
+      </button>
+    </form>
+  )}
 
 export default FreeText;
